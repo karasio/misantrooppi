a6bf145925385c32cb4e7f9dc77781748870c2e6
Katri Rasio <katri.rasio@metropolia.fi>
2019-04-24 18:16:10 +0300

    commented stuff  on kakenTesti.js and few example json answers added

--- js/kakenTesti.js
+++ js/kakenTesti.js
@@ -1,9 +1,45 @@
 'use strict';
 
 /*
-https://data-hslhrt.opendata.arcgis.com/datasets/c26bd38e37eb41eab20941cbe5dc6bd5_0.geojson
+http://api.digitransit.fi/routing/v1/routers/hsl/
+
+
+http://api.digitransit.fi/routing/v1/routers/hsl/plan?
+tällä esmes löytyy asioita:
+http://api.digitransit.fi/routing/v1/routers/hsl/plan?
+TÄMÄ MUUTTUVANA PALASENA:
+fromPlace=60.17036, 24.93768&toPlace=60.191058, 24.914124&time=1:02pm&date=05-01-2019&mode=TRANSIT,WALK&maxWalkDistance=500&arriveBy=false
  */
 
-/*
-elkdjalkdjlk  wjslkjSKLJ
- */
+button.addEventListener('click', search)
+
+
+function search(evt) {
+  let searchAdd = 'http://api.digitransit.fi/routing/v1/routers/hsl/plan?';
+  let fromAddress = document.querySelector('#fromInput').value;
+  let whereAddress = document.querySelector('#WhereInput').value;
+
+
+  // MUOKATAAN DATAA STRINGIKSI, JOTTA SEN VOI SYÖTTÄÄ API-HAUN URLIN OSAKSI
+  let start ='fromPlace=' ${crd.latitude} + ', ' + ${crd.longitude} + '';       // JOS TULEE TIEDOT SELAIMEN PAIKANNUKSESTA..
+  //let end = '&toPlace=' + JOTAKIN latitude + ', ' + JOTAKIN longitude + '';
+  // let maxWalk = '&maxWalkDistance=' + KÄYTTÄJÄN SYÖTE;
+
+
+
+  searchAdd += start + end +'mode=TRANSIT,WALK'+ maxWalk;
+  fetch(searchAdd)                              // Käynnistetään haku. Vakiometodi on GET.
+      .then(function(vastaus){        // Sitten kun haku on valmis,
+        return vastaus.json();                  // muutetaan ladattu tekstimuotoinen JSON JavaScript-olioksi
+      }).then(function(json){         // Sitten otetaan ladattu data vastaan ja
+    sortByPeople(json);                        // kutsutaan showProg-funktiota ja lähetetään ladattu data siihen parametrinä.
+  }).catch(function(error){           // Jos tapahtuu virhe,
+    console.log(error);                         // kirjoitetaan virhe konsoliin.
+  });
+}
+
+function sortByPeople(json) {
+  /*
+    let stopID = json.plan.itineraries.legs.to.stopId;
+   */
+}
