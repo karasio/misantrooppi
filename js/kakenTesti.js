e581bc394cab56a68ea743fdd7b5b558cd61fc23
Katri Rasio <katri.rasio@metropolia.fi>
2019-04-25 20:07:47 +0300

    from and to addresses are changed to coordinates and routing api searches are made with those and response is ok. comparison between nousijamaara.geojson and routing api json is in the making.

--- js/kakenTesti.js
+++ js/kakenTesti.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /*
+POHDINTAOSIO
 http://api.digitransit.fi/routing/v1/routers/hsl/
 
 
@@ -13,36 +14,101 @@ fromPlace=60.17036, 24.93768&toPlace=60.191058, 24.914124&time=1:02pm&date=05-01
 Reititys JSONissa json.plan.itineraries.legs[].from.stopCode vastaa ehkä
  nousijämäärän Lyhyt_tunn!!
 
+1. vertaile lyhyt_tunn & stopCode
+2. vertaile koordinaatit 4 numeron tarkkuudella
+else : sori ei löydy.
+
+
  */
 
-button.addEventListener('click', search)
+// getting coordinates according to address
+let haku = document.getElementById('searchButton');
+haku.addEventListener('click', getCoordinates);
+let coordinates = {};
+let boardings;
+
+fetch('nousijamaara.geojson')
+  .then(function(response) {
+    boardings = response.json();
+});
 
 
-function search(evt) {
-  let searchAdd = 'http://api.digitransit.fi/routing/v1/routers/hsl/plan?';
-  let fromAddress = document.querySelector('#fromInput').value;
-  let whereAddress = document.querySelector('#WhereInput').value;
-  let maxWalkValue = document.querySelector('#maxWalkInput').value;
+function getCoordinates() {
+  let inputFrom = document.getElementById('fromInput').value;
+  let inputTo = document.getElementById('whereInput').value;
 
-  searchAdd += fromAddress + whereAddress + maxWalkValue;     // plus mahdollisesti muita muuttujia?
+  console.log('From ' + inputFrom);
+  console.log('To ' + inputTo);
 
-  // MUOKATAAN DATAA STRINGIKSI, JOTTA SEN VOI SYÖTTÄÄ API-HAUN URLIN OSAKSI
-  let start ='fromPlace=' ${crd.latitude} + ', ' + ${crd.longitude} + '';       // JOS TULEE TIEDOT SELAIMEN PAIKANNUKSESTA..
-  //let end = '&toPlace=' + JOTAKIN latitude + ', ' + JOTAKIN longitude + '';
-  // let maxWalk = '&maxWalkDistance=' + KÄYTTÄJÄN SYÖTE;
+  fetchCoordinates(inputFrom, 'from');
+  fetchCoordinates(inputTo, 'to');
 
-  fetch(searchAdd)                              // Käynnistetään haku. Vakiometodi on GET.
-      .then(function(vastaus){        // Sitten kun haku on valmis,
-        return vastaus.json();                  // muutetaan ladattu tekstimuotoinen JSON JavaScript-olioksi
-      }).then(function(json){         // Sitten otetaan ladattu data vastaan ja
-    sortByPeople(json);                        // kutsutaan showProg-funktiota ja lähetetään ladattu data siihen parametrinä.
-  }).catch(function(error){           // Jos tapahtuu virhe,
-    console.log(error);                         // kirjoitetaan virhe konsoliin.
+}
+
+function fetchCoordinates(input, inputType) {
+  fetch(' https://nominatim.openstreetmap.org/search/' + input + '?format=json&addressdetails=1&limit=1&polygon_svg=1').
+      then(function(response) {
+        return response.json();
+      }).
+      then(function(json) {
+        tulosta(inputType, json);
+      }).
+      catch(function(error) {
+        console.log(error);
+      });
+}
+
+function tulosta(inputType, json) {
+  console.log(json);
+
+  coordinates[inputType]=json[0].boundingbox[0] + ',' + json[0].boundingbox[2];
+  if (coordinates.from && coordinates.to) {
+    console.log('toimii');
+    console.log(coordinates);
+    search();
+  }
+
+}
+
+function search() {
+  let time = getTime();
+  let searchAdd = 'http://api.digitransit.fi/routing/v1/routers/hsl/plan?fromPlace=' +
+  coordinates.from + '&toPlace=' + coordinates.to + '&time=' + time.time + '&date=' +
+      time.date + '&mode=TRANSIT,WALK&maxWalkDistance=500&arriveBy=false&showIntermediateStops=false';
+  console.log(searchAdd);
+
+  fetch(searchAdd)
+      .then(function(response){
+        return response.json();
+      }).then(function(json){
+    sortByPeople(json);
+  }).catch(function(error){
+    console.log(error);
   });
 }
 
 function sortByPeople(json) {
+  console.log('sortByPeoplessa!');
+  console.log(json);
+  const itineraries = json.plan.itineraries;
+  const boardingFeatures = boardings.features;
+  let boarderCount;
 
+/*  for(let i = 0; i < itineraries.length; i++) {
+    let itinerary = itineraries[i];
+    for (let j = 0; j < itinerary.legs.length; j++) {
+      let leg = itinerary.legs[j];
+      for (let k = 0; k < boardingFeatures.length; k++) {
+        let boardingFeature = boardingFeatures[k];
+        if (boardingFeature.properties.Lyhyt_tunn === leg.from.stopCode || TAI KOORDINAATIT TÄSMÄÄ) {
+
+        }
+      }
+    }
+  }*/
+
+
+  //let boardingByStop;
 
   /*
     let stopNames = json.plan.itineraries.legs[].from.stopCode;
--- js/samuJs.js
+++ js/samuJs.js
@@ -3,6 +3,7 @@ haku.addEventListener('click', haeTiedot);
 
 function haeTiedot() {
   let input = document.getElementById('fromInput');
+
   console.log('Haettu tiedolla ' + input.value);
   console.log('https://nominatim.openstreetmap.org/search.php?q=' + input.value);
   fetch(' https://nominatim.openstreetmap.org/search/' + input.value + '?format=json&addressdetails=1&limit=1&polygon_svg=1').
