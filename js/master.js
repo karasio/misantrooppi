ca30fbc8c843b49cbc7c4e3191b721d6a88de049
Samu <samu.rinne@metropolia.com>
2019-05-07 12:36:45 +0300

    Kommentteja lisätty ja yksi ; lisätty

--- js/master.js
+++ js/master.js
@@ -30,15 +30,16 @@ const options = {
   maximumAge: 0
 };
 
-// Funktio, joka ajetaan, kun paikkatiedot on haettu
+// function that gets called after position is located
 function success(pos) {
   const crd = pos.coords;
 
-  // Tulostetaan paikkatiedot konsoliin
+  // Print coordinates to console.
   console.log('Your current position is:');
   console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
+  //Saves coordinates for later use
   let coordinates =
       {
         lat: crd.latitude,
@@ -49,13 +50,13 @@ function success(pos) {
 
 }
 
-// Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
+// Function that is called when location does not work.
 function error(err) {
   document.getElementById('errorMsg').innerHTML = 'Paikannustietojen hakemisessa tapahtui virhe. Kirjoita lähtösijainti';
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-// Käynnistetään paikkatietojen haku
+// Starts locating user.
 navigator.geolocation.getCurrentPosition(success, error, options);
 
 let searchBtn = document.getElementById('searchButton');
@@ -78,6 +79,9 @@ fetch('nousijamaara.geojson').then(function(response) {
     });
 
 function getCoordinates() {
+  /*
+  transforms addresses to coordinates.
+   */
   document.getElementById('errorMsg').innerHTML = '<br>';
   coordinates = {};
   let inputFromValue = inputFrom.value;
@@ -91,6 +95,9 @@ function getCoordinates() {
 }
 
 function fetchCoordinates(input, inputType) {
+
+  //Sends locations to nominatim
+
   fetch(' https://nominatim.openstreetmap.org/search/' + input +
       '?format=json&addressdetails=1&limit=1&polygon_svg=1').
       then(function(response) {
@@ -106,9 +113,11 @@ function fetchCoordinates(input, inputType) {
 }
 
 function formatCoordinates(inputType, json) {
-  let coordinate
+  //Transforms locations to coordinates, places markers and starts routing
+  let coordinate;
   console.log(json);
   if(inputType === 'from' && inputFrom.value === 'Oma sijainti'){
+    // if user wants to use own location, gets located coordinates
     let coordinatesStr = localStorage.getItem('coordinates');
     let coordinates = JSON.parse(coordinatesStr);
     coordinate = {
@@ -135,6 +144,7 @@ function formatCoordinates(inputType, json) {
 let markers;
 
 function placeMarkers() {
+  //Places markers on map.
   if (markers) { // check
     map.removeLayer(markers); // remove
   }
@@ -151,6 +161,7 @@ function placeMarkers() {
 }
 
 function searchHSLRouting() {
+  // Sends coordinates to digitransit and handles routing.
   let time = getTime();
   // coordinates formatted 'lat,lon'
   let searchAdd = 'https://api.digitransit.fi/routing/v1/routers/hsl/plan?fromPlace=' +
@@ -177,6 +188,7 @@ function searchHSLRouting() {
 }
 
 function sortByPeople(json) {
+  // Gets amount of people at stops
   // debug printing
   console.log('Reititys API ');
   console.log(json);
@@ -303,6 +315,7 @@ function routeCoordinates(json) {
 
 
 function getTime() {
+  //returns current time (hh:mm and mm-dd-yyyy)
   let date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
@@ -334,11 +347,13 @@ function getTime() {
 
 function printResults(stopInfo, stopsOnRoute, json) {
   // get intermediate stop information to string (to be printed)
-  let start = stopInfo[0].startTime; //bussin lähtöaika
-  let end = stopInfo[0].endTime; // millon perillä kävelyineen
+  let start = stopInfo[0].startTime; // time when bus is on your stop (ms from 1970)
+  let end = stopInfo[0].endTime; // arrival time (ms from 1970)
   console.log("Bussi lähtee: " + getTimes(start));
   console.log("Perillä: "+ getTimes(end));
-  let amountOfPeople = numberOfPeople(stopInfo[0].boarderCount);
+  let amountOfPeople = numberOfPeople(stopInfo[0].boarderCount); // calculates amount of people at your stop currently.
+
+  //Prints first options information.
   let stopsOnRouteList =
       '<div id="lower">' +
       '<ul class="option">' +
@@ -348,10 +363,13 @@ function printResults(stopInfo, stopsOnRoute, json) {
       '<li> Pysäkki: '+ stopInfo[0].code+'</li>' +
       '<li>Ihmismäärä: '+ amountOfPeople +'</li>' +
       '<li>Lähtöaika: '+ getTimes(start) +'</li>';
+
+  // Goes through all of the stops, prints those and vehicles used etc.
   for (let j=0; j < stopsOnRoute.length; j++) {
     let vehicleClass = '';
     let stopsOnOneRoute = stopsOnRoute[j];
     for (let k = 0; k < stopsOnOneRoute.length; k++) {
+      // determines vechileClass which is used to get correct image infront of legs.
       switch (stopsOnOneRoute[k].mode) {
         case 'WALK':
           vehicleClass ='walk';
@@ -373,6 +391,7 @@ function printResults(stopInfo, stopsOnRoute, json) {
           break;
       }
       let x = k+1;
+      // prints "----" every time you change vehicle or start walking again.
       if((k > 0) && (x < stopsOnOneRoute.length) && (stopsOnOneRoute[k].name === stopsOnOneRoute[x].name)) {
         continue;
       } else if (k === stopsOnOneRoute.length-1) {
@@ -388,12 +407,11 @@ function printResults(stopInfo, stopsOnRoute, json) {
       }
     }
     if (j < stopsOnRoute.length-1) {
-      start = stopInfo[j+1].startTime; //bussin lähtöaika
-      end = stopInfo[j+1].endTime; //millon perillä kävelyineen
-      //console.log("Bussi lähtee: " + getTimes(start));
-      //console.log("Perillä: "+ getTimes(end));
+      start = stopInfo[j+1].startTime; // when bus is at your stop (ms from 1970)
+      end = stopInfo[j+1].endTime; // arrival time (ms from 1970)
       amountOfPeople = numberOfPeople(stopInfo[j+1].boarderCount);
       stopsOnRouteList +=
+          // prints infos for options 2-3 if needed.
           '<li>Perillä: ' + getTimes(stopInfo[j+1].endTime) + '</li>' +
           '</ul>' +
           '</li>' +
@@ -407,6 +425,7 @@ function printResults(stopInfo, stopsOnRoute, json) {
     }
   }
   stopsOnRouteList +=
+      // prints when you will arrive to your destination
       '<li>Perillä: ' + getTimes(end) + '</li>' +
       '</ul>' +
       '</div>';
@@ -415,6 +434,7 @@ function printResults(stopInfo, stopsOnRoute, json) {
   let inputToValue = toTitleCase(inputTo.value);
 
   let aside;
+  // makes sure that correct result is visible when user searches again.
   if (document.getElementById("results") != null) {
     aside = document.getElementById('results');
   }else {
@@ -426,6 +446,7 @@ function printResults(stopInfo, stopsOnRoute, json) {
 
   let info = document.getElementById('info');
   let moreInfo = document.getElementById('moreInfo');
+  // prints some infromtion above results like what locations you used to get results.
   info.innerHTML = '<div class="upper"><h3>Matkan tiedot</h3>' +
       'Kohteesta: ' + inputFromValue + '<br>' +
       'Kohteeseen: ' + inputToValue + '<br>' +
@@ -437,7 +458,7 @@ function printResults(stopInfo, stopsOnRoute, json) {
       '<div id="hideText" >Piilota pysäkit<img id=hideTextIcon src="media/closesmall.png"></div></div>' + stopsOnRouteList;
 
   getRouteOptionElements(json);
-
+  // Determines if "result" or "bigresult" should be visible.
   let showStopText = document.getElementById('showText');
   showStopText.addEventListener('click', function() {
     aside.setAttribute('id', 'bigResults');
@@ -464,6 +485,7 @@ function toTitleCase(str) {
   );
 }
 function numberOfPeople(peoplePerDay){
+  // Calculates how many people are waiting at stop currently and returns it.
   let time = getTime();
   let hour = time.hours;
   if (peoplePerDay === " ei tiedossa"){
@@ -481,6 +503,7 @@ function numberOfPeople(peoplePerDay){
   }
 }
 function getTimes(time) {
+  //take milliseconds as an argument and returns time (hh:mm) for arrival and leaving times.
   let date = new Date(time);
   let hours = date.getHours();
   let minutes = date.getMinutes();
@@ -503,7 +526,6 @@ function drawRoute(json, index) {
   routeLayer = L.layerGroup().addTo(map);
 
   console.log(json);
-  //MUISTA VAIHTAA MUUTTUJAKSI
   let legs = json.plan.itineraries[index].legs;
   for (let i = 0; i < legs.length; i++) {
     let fromCrd = {
